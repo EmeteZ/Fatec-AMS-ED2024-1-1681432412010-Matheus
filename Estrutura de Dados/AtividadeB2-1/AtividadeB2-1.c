77820a675cb17b423265d515ff8c35af5571e927
Matheus <125473215+EmeteZ@users.noreply.github.com>
2024-04-22 22:14:29 -0300

    Update AtividadeB2-1.c

--- Estrutura de Dados/AtividadeB2-1/AtividadeB2-1.c
+++ Estrutura de Dados/AtividadeB2-1/AtividadeB2-1.c
@@ -1,41 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_STACK_SIZE 100
+
+typedef struct {
+    char* items[MAX_STACK_SIZE];
+    double values[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+void push(Stack *s, char* value, double num) {
+    if (s->top == MAX_STACK_SIZE - 1) {
+        printf("Erro: Pilha cheia.\n");
+        exit(EXIT_FAILURE);
+    }
+    s->items[++(s->top)] = value;
+    s->values[s->top] = num;
+}
+
+char* pop(Stack *s, double *num) {
+    if (s->top == -1) {
+        printf("Erro: Pilha vazia.\n");
+        exit(EXIT_FAILURE);
+    }
+    *num = s->values[s->top];
+    return s->items[(s->top)--];
+}
+
+double calcular(char op, double num1, double num2) {
+    switch (op) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 == 0) {
+                printf("Erro: Divisão por zero.\n");
+                exit(EXIT_FAILURE);
+            }
+            return num1 / num2;
+        default:
+            printf("Operador inválido.\n");
+            exit(EXIT_FAILURE);
+    }
+}
 
 int main() {
-  int num1, num2;
-  char operador;
-  float resultado;
-resultado = 0.0;
-  printf("Faça seu calculo em formato RPN: ");
-  scanf("%d %d %c", &num1, &num2, &operador);
+    Stack pilha;
+    pilha.top = -1;
 
-  switch (operador) {
-  case '+':
-    resultado = num1 + num2;
-    printf("Soma: %f\n", resultado);
-    printf("Formula Algebrica: %d %c %d\n", num1 
-      ,operador,num2 );
-    break;
-  case '-':
-    resultado = num1 - num2;
-    printf("Diferença: %f\n", resultado);
-    printf("Formula Algebrica: %d %c %d\n", num1 
-      ,operador,num2 );
-    break;
-  case '*':
-    resultado = num1 * num2;
-    printf("Produto: %f\n", resultado);
-    printf("Formula Algebrica: %d %c %d\n", num1 
-      ,operador,num2 );
-    break;
-  case '/':
-    resultado = num1 / num2;
-    printf("Sobra: %f\n", resultado);
-    printf("Formula Algebrica: (%d %c %d)\n", num1 
-      ,operador,num2 );
-    break;
-  default:
-    printf("Escolha invalida\n");
-  }
+    char entrada[100];
+    printf("Digite a expressão RPN (use espaços entre os elementos): ");
+    fgets(entrada, sizeof(entrada), stdin);
 
-  return 0;
+    char *ptr = entrada;
+    while (*ptr != '\0') {
+        if (isspace(*ptr)) {
+            ptr++;
+            continue; 
+        }
+        if (isdigit(*ptr)) {
+            char* num = (char*) malloc(20 * sizeof(char));
+            int i = 0;
+            while (isdigit(*ptr) || *ptr == '.') {
+                num[i++] = *ptr++;
+            }
+            num[i] = '\0';
+            push(&pilha, num, atof(num));
+        } else if (*ptr == '+' || *ptr == '-' || *ptr == '*' || *ptr == '/') {
+            double num2, num1;
+            char* str2 = pop(&pilha, &num2);
+            char* str1 = pop(&pilha, &num1);
+            char* expr = (char*) malloc((strlen(str1) + strlen(str2) + 4) * sizeof(char));
+            sprintf(expr, "(%s %c %s)", str1, *ptr, str2);
+            push(&pilha, expr, calcular(*ptr, num1, num2));
+            ptr++;
+        } else {
+            printf("Caractere inválido na entrada: '%c'\n", *ptr);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (pilha.top != 0) {
+        printf("Erro: Entrada inválida.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    double resultadoNum;
+    char* resultadoExpr = pop(&pilha, &resultadoNum);
+    printf("Resultado: %s = %.2f\n", resultadoExpr, resultadoNum);
+
+    return 0;
 }
